21b83ebb75105c6c99d9f4cb1abc4de4a289bfe5
htomeload <57021229@live.up.ac.th>
2017-11-13 00:55:56 +0700

    Add beacons to in-app db

--- src/provider/beacons/beacons.ts
+++ src/provider/beacons/beacons.ts
@@ -1,5 +1,5 @@
 export class BeaconsDBProvider{
-	protected beacons: Array<{identifier: string, uuid: string, major: number, minor: number}>;
+	protected beacons: Array<{identifier: string, uuid?: string, major?: number, minor?: number}>;
 	
 	constructor(){
 		this.beacons = [
@@ -14,6 +14,12 @@ export class BeaconsDBProvider{
 				uuid: "FDA50693-A4E2-4FB1-AFCF-C6EB07647825",
 				major: 10035,
 				minor: 56498,
+			},
+			{
+				identifier: "88:4A:EA:6C:34:E8",
+			},
+			{
+				identifier: "50:8C:B1:75:23:9D",
 			},
 		];
 	}
